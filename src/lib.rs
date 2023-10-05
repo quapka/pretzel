db1f16619928d355fe2add3c702ed167b470dd12
quapka <quapka@gmail.com>
2023-10-05 14:16:13 +0200

    Convert to BigUint from Uint right after prime generation
    Using two different libraries for handling Big Integers is not quite
    ergonomic. Since we are after functional implementation rather than
    bulletproofed one, we switch to num-bigint-dig types once crypto-primes
    generates the safe primes for us.

--- src/lib.rs
+++ src/lib.rs
@@ -2,97 +2,149 @@
 // NOTE: what is the difference between # and #!?
 #![allow(unused_imports)]
 // use errors::{Error, Result};
+#[cfg(not(test))]
+use log::info;
+
 use num_bigint::*;
+#[cfg(test)]
+use std::{println as info, println as warn};
+use thiserror::Error;
+// use std::error::Error
 // use num_modular::*;
 use crypto_bigint::*;
 use crypto_primes::*;
 // use num_prime::nt_funcs::*;
-use num_traits::{One, Zero};
+use num_integer::Integer;
+use num_traits::{CheckedSub, One, Zero};
 use rand::prelude::*;
 use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng, ChaCha8Rng};
 use rand_core::CryptoRngCore;
 use rsa::{RsaPrivateKey, RsaPublicKey};
+use std::any::type_name;
+use std::cmp::Ordering;
+use std::ops::{Div, Mul, Shr};
 
-pub fn generate_primes(modulus_bit_length: usize) -> (U2048, U2048) {
-    let p: U2048 = generate_safe_prime(Some(modulus_bit_length / 2));
-    let mut q: U2048 = generate_safe_prime(Some(modulus_bit_length / 2));
-    while p == q {
-        q = generate_safe_prime(Some(modulus_bit_length / 2));
-    }
-    (p, q)
-}
+// pub fn generate_primes(modulus_bit_length: usize) -> (U2048, U2048) {
+//     let p: U2048 = generate_safe_prime(Some(modulus_bit_length / 2));
+//     let mut q: U2048 = generate_safe_prime(Some(modulus_bit_length / 2));
+//     while p == q {
+//         q = generate_safe_prime(Some(modulus_bit_length / 2));
+//     }
+//     (p, q)
+// }
 
 #[derive(Debug)]
 pub struct RSAThresholdPrivateKey {
-    p: U2048,
-    q: U2048,
-    d: U4096,
-    m: U4096,
-    e: U4096,
+    p: BigUint,
+    q: BigUint,
+    d: BigUint,
+    m: BigUint,
+    e: BigUint,
 }
 
 #[derive(Debug)]
 pub struct RSAThresholdPublicKey {
-    n: U4096,
-    e: U4096,
+    n: BigUint,
+    e: BigUint,
 }
 
-impl RSAThresholdPrivateKey {
-    fn get_public(&self) -> RSAThresholdPublicKey {
-        RSAThresholdPublicKey {
-            n: self.p * self.q,
-            e: self.e,
-        }
-    }
+#[derive(Error, Debug)]
+pub enum KeyGenError {
+    // #[error("The provided bit_length '{found:?}' was greater than the expected '{expected:?}'.")]
+    // UnexpectedBitLength { expected: usize, found: usize },
+    #[error("Too Big")]
+    TooBig,
+    #[error("Too Small")]
+    TooSmall,
+    #[error("No Inverse")]
+    NoInverse,
+    #[error("The group is too big")]
+    GroupTooBig,
+    #[error("Bit length does not match")]
+    BitLength,
 }
+
+#[derive(Error, Debug)]
+pub enum PolynomialError {
+    #[error("No coefficients/polynomial provided")]
+    NoCoefficients,
+}
+
+// impl RSAThresholdPrivateKey {
+//     fn get_public(&self) -> RSAThresholdPublicKey {
+//         RSAThresholdPublicKey {
+//             n: self.p * self.q,
+//             e: self.e,
+//         }
+//     }
+// }
+//
+// fn print_type_of<T>(_: &T) {
+//     println!("{:?}", std::any::type_name::<T>())
+// }
 
 /// `n_parties` equals the `l` parameter from the paper
-pub fn key_gen(bit_length: usize, l: usize, k: usize, t: usize) -> RSAThresholdPrivateKey {
+pub fn key_gen(
+    bit_length: usize,
+    l: usize,
+    k: usize,
+    t: usize,
+) -> Result<RSAThresholdPrivateKey, KeyGenError> {
     // FIXME add bounds on l, k and t
-    let (p, q) = generate_primes(bit_length);
-    let e: U4096 = U4096::from_u32(0x10001);
-    // FIXME worry about the sizes later
-    // if l > e {
-    //     println!(
-    //         "The number of parties: {} is greater than expected ({})",
-    //         l, e
-    //     );
-    //     panic!();
-    // };
+    let (p, q) = match generate_p_and_q(bit_length) {
+        Ok((p, q)) => (p, q),
+        Err(e) => return Err(e),
+    };
+    let e: BigUint = BigUint::new(vec![0x10001]); // 65537
 
-    let n = p * q;
-    let one = Uint::ONE;
-    // FIXME remove unwraps
-    // shift right by one to divide by 2
-    let pp = p.checked_sub(&one).unwrap().shr_vartime(1);
-    let qq = q.checked_sub(&one).unwrap().shr_vartime(1);
+    // FIXME: compare against e directly
+    if l > 65537 {
+        return Err(KeyGenError::GroupTooBig);
+    };
 
-    let m: U4096 = pp * qq;
+    let n = p.clone().mul(&q);
+    // FIXME code without unwraps
+    let p_prime = &p.checked_sub(&BigUint::one()).unwrap().shr(1);
+    let q_prime = &q.checked_sub(&BigUint::one()).unwrap().shr(1);
 
-    println!("p: {}", p);
-    println!("m: {}", m.bits());
+    let m = p_prime.mul(q_prime);
+    let dd = match e.clone().mod_inverse(&m) {
+        Some(value) => value,
+        None => return Err(KeyGenError::NoInverse),
+    };
+    assert!(dd.cmp(&BigInt::zero()) == Ordering::Greater);
 
-    RSAThresholdPrivateKey {
+    let d: BigUint = match dd.to_biguint() {
+        Some(value) => value,
+        None => return Err(KeyGenError::NoInverse),
+    };
+
+    Ok(RSAThresholdPrivateKey {
         p: p,
         q: q,
-        d: e.inv_odd_mod(&n).0,
+        d: d,
         m: m,
         e: e,
-    }
+    })
 }
 
-fn nm_evaluate_polynomial_mod(value: BigUint, coeffs: &Vec<BigUint>, modulus: &BigUint) -> BigUint {
-    let mut result = BigUint::zero();
-    let mut x: BigUint;
-    // NOTE is cloning necesary here?
-    let mut bk: BigUint = coeffs[coeffs.len() - 1].clone();
-    // let mut bk: BigUint = coeffs[coeffs.len() - 1].clone();
-    for i in (0..=coeffs.len() - 2).rev() {
-        bk = bk * &value + &coeffs[i];
+fn evaluate_polynomial_mod(
+    value: BigUint,
+    coeffs: &Vec<BigUint>,
+    modulus: &BigUint,
+) -> Result<BigUint, PolynomialError> {
+    let mut prev: BigUint = match coeffs.last() {
+        Some(last) => last.clone(),
+        None => return Err(PolynomialError::NoCoefficients),
+    };
+
+    for next in coeffs.iter().rev().skip(1) {
+        // TODO what multiplication and addition is used for * and +, should we call functions
+        // mul and add instead?
+        prev = prev * &value + next;
     }
-    // FIXME doing modulus like this seems very fishy
-    let rem = bk.modpow(&BigUint::one(), modulus);
-    rem
+    let rem = prev.mod_floor(modulus);
+    Ok(rem)
 }
 
 // fn evaluate_polynomial_mod(value: U4096, coeffs: Vec<U4096>, modulus: U4096) -> U4096 {
@@ -113,28 +165,17 @@ fn nm_evaluate_polynomial_mod(value: BigUint, coeffs: &Vec<BigUint>, modulus: &B
 // }
 
 fn generate_secret_shares(key: RSAThresholdPrivateKey, l: usize, k: usize) -> Vec<BigUint> {
-    let mut a_coeffs = vec![U2048::ONE; k];
     // generate random coefficients
     let mut rng = ChaCha20Rng::from_entropy();
-    let modulus = NonZero::from_uint(key.m);
-    let mut a_coeffs: Vec<U4096> = a_coeffs
-        .iter()
-        .map(|a| U4096::random_mod(&mut rng, &modulus))
+    let mut a_coeffs: Vec<BigUint> = (1..=(k - 1))
+        .map(|_| rng.gen_biguint_below(&key.m))
         .collect();
-
+    // fix a_0 to the private exponent
     a_coeffs[0] = key.d;
-
-    let nbi_a_coeffs: Vec<BigUint> = a_coeffs
-        .iter()
-        .map(|a| BigUint::from_bytes_be(&a.to_be_bytes()))
+    // calculate the individual shares
+    let shares: Vec<BigUint> = (1..=l)
+        .map(|i| evaluate_polynomial_mod(i.into(), &a_coeffs, &key.m).unwrap())
         .collect();
-
-    let nbi_m = BigUint::from_bytes_be(&key.m.to_be_bytes());
-
-    let mut shares: Vec<BigUint> = vec![];
-    for i in 1..=l {
-        shares.push(nm_evaluate_polynomial_mod(i.into(), &nbi_a_coeffs, &nbi_m));
-    }
     shares
 }
 
@@ -143,16 +184,57 @@ fn generate_verification(key: RSAThresholdPrivateKey) {
     // gen_biguint_below(key.
 }
 
+// Based on this API the `bit_length` should not be divided, but instead
+// the division shouldbe handled by the key gen caller
+fn generate_p_and_q(bit_length: usize) -> Result<(BigUint, BigUint), KeyGenError> {
+    let min_bit_length = 3;
+    let max_bit_length = 16384;
+    let half_bit_length = bit_length / 2;
+
+    if half_bit_length < min_bit_length {
+        // Trying to prevent the following panic!
+        // https://docs.rs/crypto-primes/latest/src/crypto_primes/presets.rs.html#85
+        return Err(KeyGenError::TooSmall);
+    }
+    if half_bit_length > max_bit_length {
+        return Err(KeyGenError::TooBig);
+    }
+
+    // Use the largest available types for the initial primes generates
+    // Generate two distinct safe probably primes
+    info!("Generating p prime..");
+    // FIXME From experimenting it seems that larger values mean much slower generation times
+    // So ideally we would pick the U type based on the half_bit_length
+    // E.g. U2048 vs U16384
+    let crypto_p: U2048 = generate_safe_prime(Some(half_bit_length));
+    info!("Generating q prime..");
+    let mut crypto_q: U2048 = generate_safe_prime(Some(half_bit_length));
+    while crypto_p == crypto_q {
+        info!("p == q, recalculating q");
+        crypto_q = generate_safe_prime(Some(half_bit_length));
+    }
+
+    // FIXME: I am a bit unsure about the converting between crypto-bigint and num-bigint
+    let p = BigUint::from_bytes_be(&crypto_p.to_be_bytes());
+    let q = BigUint::from_bytes_be(&crypto_q.to_be_bytes());
+
+    if p.bits() != half_bit_length || q.bits() != half_bit_length {
+        return Err(KeyGenError::BitLength);
+    }
+
+    Ok((p, q))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rand::prelude::*;
     use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng, ChaCha8Rng};
+    use std::iter::zip;
+    // use test_log::test;
 
     #[test]
     fn test_evaluate_polynomial() {
-        // let modulus = U4096::from_u32(64);
-        // let coeffs = vec![U4096::ONE; 32];
         let coeffs = vec![
             BigUint::from(17u32),
             BigUint::from(63u32),
@@ -160,7 +242,7 @@ mod tests {
         ];
         let modulus = BigUint::from(127u32);
         assert_eq!(
-            nm_evaluate_polynomial_mod(BigUint::from(2u32), &coeffs, &modulus),
+            evaluate_polynomial_mod(BigUint::from(2u32), &coeffs, &modulus).unwrap(),
             BigUint::from(16u32)
         );
     }
@@ -173,7 +255,7 @@ mod tests {
         let t = 1;
         let bit_length = 32;
         let sk = key_gen(bit_length, l, k, t);
-        println!("{:?}", sk.get_public());
+        // println!("{:?}", sk.get_public());
     }
 
     #[test]
@@ -182,7 +264,7 @@ mod tests {
         let k = 2;
         let t = 1;
         let bit_length = 128;
-        let sk = key_gen(bit_length, l, k, t);
+        let sk = key_gen(bit_length, l, k, t).unwrap();
         let shares = generate_secret_shares(sk, l, k);
         println!("shares: {:?}", shares);
     }
@@ -202,13 +284,50 @@ mod tests {
     }
 
     #[test]
-    fn from_crypto_to_num() {
-        let p: U4096 = generate_safe_prime(Some(32));
+    fn test_key_gen() {
+        // another_key_gen(100000);
 
-        println!("{:?}", p);
+        // Given bits
+        // let bit_length = 512;
+        // let x: U8 = match bit_length {
+        //     ..=32 => generate_safe_prime(Some(bit_length)),
+        //     _ => generate_safe_prime(Some(256)),
+        // };
+
+        // assert_eq!(x.bits(), 32);
+    }
+
+    #[test]
+    fn from_crypto_to_num() {
+        let p: U256 = generate_safe_prime(Some(256));
+        // let value = 32;
+        // let x = match value {
+        //     ..=32 => U64::generate_safe_prime(Some(value)),
+        //     _ => U
+        // }
+
+        // println!("{:?}", p);
         let bytes = p.to_be_bytes();
-        println!("{:?}", bytes);
-        println!("{:?}", BigUint::from_bytes_be(&bytes));
+        // println!("{:?}", bytes);
+        let nb_p = BigUint::from_bytes_be(&bytes);
+        // println!("{:?}", nb_p.to_bytes_be());
+
+        for (a, b) in zip(p.to_be_bytes(), nb_p.to_bytes_be()) {
+            assert_eq!(a, b);
+        }
+    }
+
+    #[test]
+    fn generating_small_primes_errors() {
+        assert!(generate_p_and_q(1).is_err());
+
+        let (p, q) = generate_p_and_q(100).unwrap();
+
+        assert!(p > BigUint::one());
+        assert!(q > BigUint::one());
+
+        println!("{:?}", p.to_bytes_be());
+        println!("{:?}", q.to_bytes_be());
     }
 
     #[test]
@@ -224,5 +343,11 @@ mod tests {
         // let modulus = 2048;
         // let key = key_gen(&mut rng, modulus);
         // println!("{}", key.bits());
+    }
+
+    #[test]
+    fn test_ordering() {
+        println!("{:?}", BigUint::zero().cmp(&BigUint::one()));
+        println!("{:?}", BigUint::one().cmp(&BigUint::zero()));
     }
 }
